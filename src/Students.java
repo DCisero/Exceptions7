755b13757a4c0536deaf403b9432bfa1f3ed49cc
DCisero <deshaylacisero@gmail.com>
2017-07-20 11:33:49 -0400

    second commit

--- src/Students.java
+++ src/Students.java
@@ -14,9 +14,9 @@ public class Students {
         String answer = "Y";
 
         //create the arrays
-        String[] classNames = {"Avion", "Brielle", "Chanel"};
-        String[] homeTowns = {"Arizona", "Boston", "California"};
-        String[] favFoods = {"apple", "banana", "carrot"};
+        String[] classNames = {"Avion", "Brielle", "Chanel", "Kim", "Regina", "Nancy", "Daisy", "Chance", "Erykah", "Mari", "Lisia", "Manny", "Sarah", "Danny","Tiffany","Allie","Walter","Candance","Fannie","Naomi"};
+        String[] homeTowns = {"Arizona", "Boston", "California", "Kansas", "Rhode Island", "New York", "Dakota", "Chicago", "New Englad", "Maine", "Las Vegas", "Mayland", "Seattle", "Denver", "Tennesse", "Alabama", "Wiscosin", "Connecticut", "Florida", "Nebraska"};
+        String[] favFoods = {"apple", "banana", "carrot", "kale", "rice", "noodles", "donut", "chocolate", "eggplant", "mango", "licorice", "madarian orange", "sandwich", "dumplings", "toffee", "apricot", "watermelon", "coconut", "fish", "nachos"};
 
 
         System.out.println("Welcome to the TechHire 2017 class!");
@@ -24,7 +24,7 @@ public class Students {
 
         do {
             try {
-                System.out.print("Which student would you like to learn about? Please enter (1-3): ");
+                System.out.print("Which student would you like to learn about? Please enter (1-20): ");
                 System.out.println();
                 studentNum = scan.nextInt();
                 scan.nextLine();
@@ -36,7 +36,6 @@ public class Students {
                 System.out.println();
                 userLearns = scan.nextLine();
 
-            //try {
                 if (userLearns.contains("home")) {
                     System.out.println("Your student's hometown is " + homeTowns[studentNum - 1] + ".");
                     System.out.println();
@@ -46,20 +45,20 @@ public class Students {
                     System.out.println("Your student's ultimate favorite food is " + favFoods[studentNum - 1] + ".");
                     System.out.println();
                 }
-            //}
-            //catch (InputMismatchException e) {
-                //System.out.println("This data is invalid, please try again!");
-            //}
 
                 System.out.print("Would you like to learn more about another student? Enter (Y/N)");
                 System.out.println();
                 answer = scan.nextLine();
 
-            } catch(ArrayIndexOutOfBoundsException e){
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("This is not a valid student number, please try again!");
                 System.out.println();
+            } catch (InputMismatchException e) {
+                System.out.println("This data is invalid, please try again!");
+                scan.nextLine();
             }
-        } while (answer.equalsIgnoreCase("Y")) ;
+        } while (answer.equalsIgnoreCase("Y"));
+
     }
 }
 
